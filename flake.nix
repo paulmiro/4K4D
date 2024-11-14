22133245ff581c3f74e437a9a0c1ee331e87ad07
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-11-14 12:23:16 +0100

    build with 32 cores

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    cores = 32;
   };
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
