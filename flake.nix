8f5c0b1dddc535f37fb3669c5918b8b4e6c46618
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-11-14 12:16:29 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cuda-maintainers.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = (import nixpkgs {
+          config.allowUnfree = true;
+          config.cudaSupport = true;
+          inherit system;
+        });
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ python312 ] ++
+            (with pkgs.python312Packages; [
+              tiny-cuda-nn
+              pytorch3d
+              torch
+            ]);
+        };
+      });
+    };
+}
