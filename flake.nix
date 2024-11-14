a717c71fb3f7e87f23d9bdeffeeaf5b11f2e469f
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-11-14 12:37:15 +0100

    allow 2 jobs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    cores = 32;
+    cores = 24;
+    max-jobs = 2;
   };
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
